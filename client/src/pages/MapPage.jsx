d10e0353017f62cb6582920a2de8800e635b80db
Porcari-Karim <porcari.karim1@gmail.com>
2024-11-22 12:32:29 +0100

    added karim messy code

---
+++ client/src/pages/MapPage.jsx
@@ -0,0 +1,13 @@
+import "maplibre-gl/dist/maplibre-gl.css";
+import 'maplibre-react-components/style.css';
+
+import { RMap, RNavigationControl, RSource, RLayer } from 'maplibre-react-components';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+function MapPage() {
+    return (  );
+}
+
+export default MapPage;
